5d811fa521c9679cae2ef10ea3177bc59fb6fa52
raymondmarechu07-ui <raymondmarechu07@gmail.com>
2026-09-15 20:45:46 +0100

    Delete app directory

--- app/api/auth/logout/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-export async function POST() {
-  const response = NextResponse.json({ ok: true });
-  response.cookies.delete('st_session');
-  return response;
-}
